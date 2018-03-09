a67ff81927794b9bb0c1ba70c35c917f51526e41
Plessard <plessard2157@gmail.com>
2018-03-09 17:46:11 -0500

    Plant model
    Added code for the satellite model. right now we are the wheel moment of inertia.

---
+++ Models/constants.m
@@ -0,0 +1,6 @@
+%I is the moment of inertia without the wheel
+I = diag(113209925.22e-6, 157664078.25e-6, 178851595.39e-6);
+%Iw is the moment of inertia of the wheel
+Iw = ;
+%Is is the total moment of inertia including the wheel
+Is = I + Iw;
---
+++ Models/eq_motion.m
@@ -0,0 +1,17 @@
+function dx = eq_motion(x, L)
+
+% load constants    
+constants;
+
+% get x into better form
+omega_sat = x(1:3);
+omega_wheel = x(4);
+epsilon = x(5:7);
+eta = x(8);
+% L is disturbance torque data imported
+us = -L(3);
+
+dx = [inv(Is)*(-cross(omega_sat)*Is*omega_sat+(-cross(omega_sat)*Is*(x(3)+omega_wheel)-us)*[0;0;1]);%OMEGA_SAT_DOT
+      inv(Iw)*us;%including w(3) term, it becomes: inv(Iw)*us-w(3)_dot      OMEGA_WHEEL_DOT
+      0.5*omega_sat*(eta*eye(3)+cross(epsilon)); %EPSILON_DOT
+      -0.5*omega_sat*epsilon']; %ETA_DOT
